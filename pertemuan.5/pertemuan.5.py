4b197a8048a78ed25237270d82b0f5d294923a90
reza haikal <ezaayt07@gmail.com>
2025-10-15 01:06:04 +0800

    first commit

---
+++ pertemuan.5/pertemuan.5.py
@@ -0,0 +1,90 @@
+# praktikum = ["Mahasiswa", 20, True, 45.10, [ "APD", 25]]
+
+# print 
+
+# studyclub = ["Data Science", "Robotics", "Multimedia", "Network"]
+# print(studyclub)
+# studyclub [3]
+
+# matakuliah = ['PTI', 'APD','Kalkulus','Diskrit']
+# print(matakuliah)
+# # menghapus elemen pada indeks ke-2, yakni "Kalkulus"
+# del matakuliah[2]
+# print(matakuliah)
+
+# matakuliah = ['PTI', 'APD','Kalkulus','Diskrit']
+# print(matakuliah)
+# # menghapus elemen dengan nilai "Kalkulus"
+# matakuliah.remove('Kalkulus')
+# print(matakuliah)
+
+# matakuliah = ['PTI', 'APD','Kalkulus','Diskrit']
+# print(matakuliah)
+# # Menghapus & mengambil elemen 'Kalkulus' pada indeks ke-2
+# sampah = matakuliah.pop(2)
+# print(matakuliah)
+# print(sampah)
+
+# # list
+# matakuliah = ['PTI', 'APD','Kalkulus','Diskrit','Bahasa Inggris',
+# 'Orsikom','Basis Data']
+# # Menampilkan list mulai dari indeks 1 hingga 5 dengan loncatan 2
+# print(matakuliah[0:6:3])
+
+# # List
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# # menggabungkan kedua list dengan operator (+)
+# c = a + b
+# print(c)
+
+# # List
+# a = ["teknik", "informatika"]
+# # mengulang isi list dengan operator (*) sebanyak 3 kali
+# c = a*3
+# print(c)
+
+# # Membuat Nested List
+# kelas = [
+# ["Ridho", "Lian", "Nabil" ["dodol" "makan " ]],
+# ["Daffa", "Dante", "Santoso"],
+# ["Pernanda", "Riyadi", "Ahnaf"],
+# ]
+
+# print(kelas[0][3][0])
+
+# # list mahasiswa
+# mahasiswa = [["Daffa", "Dante", "Santoso"], ["Pernanda", "Triya", "Ahnaf"]]
+# # perulangan for untuk mendapatkan semua elemen
+# for i in mahasiswa:
+#     print (i)
+#     for j in i :
+#         print (j)
+
+# anggota = ("riyadi", 20, True, 3.96, ["APD",25],("samarinda",12))
+# #anggota[0] = "lian"
+
+# anggota = list(anggota)
+
+# anggota.insert()
+# print(anggota)
+
+# # tuple
+# tugas = ('aku', 'saya','dia')
+
+# # beri variabel setiap values
+# (P, o, i) = tugas
+# print(*P)
+# print(*o)
+# print(i)
+
+# barang = (['triangle','bola'], ['meja', 'handphone'], ['televisi', 'laptop'])
+
+# for  nama_barang in barang:
+#     print (nama_barang)
+#     (barang1, barang2) = nama_barang
+#     print(barang1)
+#     print(barang2)
+
+# mahasiswa = [["reja","riji"],["romi","rahmat"],("muler")]
+# for mahasiswa in 
